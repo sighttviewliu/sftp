edace9fbf5119a71c5da7017f588349c1d68891a
sighttviewliu <denchance@gmail.com>
2019-06-04 12:40:44 +0800

    title: sftp comment: add run.sh script

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 ## Simplest docker run example
-docker run -p 22:22 -d atmoz/sftp foo:pass:::upload
+docker -D run -p 22:22 -d atmoz/sftp foo:pass:::upload
+
+## For testing
+docker -D run -it --rm --name mysftp -p 22:22 atmoz/sftp foo:pass:::upload 
 
 ## Sharing a directory from your computer
-docker run \
+docker -D run \
     -v /host/upload:/home/foo/upload \
     -p 2222:22 -d atmoz/sftp \
     foo:pass:1001
+
+## For testing
+docker -D run -it --rm --name mysftp -p 2222:22 -v $(pwd)/upload:/home/foo/upload atmoz/sftp foo:pass:1001
 
 ## Logging in
 sftp -P 2222 foo@<host-ip>
